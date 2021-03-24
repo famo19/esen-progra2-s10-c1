c7d8df7a3ec128f582f6bfabce2f4cdb4298d655
Francisco Alejandro Murillo Oviedo <murillo.francisco2001@gmail.com>
2021-03-23 20:49:21 -0600

    Practica

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function imgSlider(drink){
+    //let parent = drink
+    parent.classList.add("li:hover")
 }
 
 function changeBgColor(color){
+    let parent = document.querySelectorAll("ul.thumb")
+    parent.style.backgroundColor = color
 }
