14d829421d7632f435264ae49f68f84359af7737
Francisco Alejandro Murillo Oviedo <murillo.francisco2001@gmail.com>
2021-03-23 21:27:46 -0600

    Fixed

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function imgSlider(drink){
-    //let parent = drink
-    parent.classList.add("li:hover")
-}
 
 function changeBgColor(color){
-    let parent = document.querySelectorAll("ul.thumb")
-    parent.style.backgroundColor = color
+    let parent = document.querySelector("section")
+    parent.style.background = color
 }
